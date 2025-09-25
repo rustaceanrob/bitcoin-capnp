50a81d54ff5e79349c0b2694d2c99d41553fe44e
rustaceanrob <rob.netzke@gmail.com>
2025-09-25 08:43:40 +0100

    Re-export all modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,5 @@
 capnp::generated_code!(pub mod init_capnp);
+capnp::generated_code!(pub mod common_capnp);
+capnp::generated_code!(pub mod echo_capnp);
+capnp::generated_code!(pub mod proxy_capnp);
+capnp::generated_code!(pub mod mining_capnp);
